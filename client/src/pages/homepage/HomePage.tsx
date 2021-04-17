1bacc415f431d983b9df427d8004fb45aed30486
Sam <student979@live.com>
2021-04-17 15:31:14 -0400

    add homepage

---
+++ client/src/pages/homepage/HomePage.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Stack,
+  Text,
+  Link,
+  FontWeights,
+  IStackTokens,
+  PrimaryButton,
+  DefaultButton,
+} from "@fluentui/react";
+import logo from "./logo.svg";
+import "./HomePage.css";
+
+const boldStyle = { root: { fontWeight: FontWeights.semibold } };
+const stackTokens: IStackTokens = { childrenGap: 15 };
+
+export const HomePage: React.FunctionComponent = () => {
+  return (
+    <Stack
+      horizontalAlign="center"
+      verticalAlign="center"
+      verticalFill
+      styles={{
+        root: {
+          width: "960px",
+          margin: "0 auto",
+          textAlign: "center",
+          color: "#605e5c",
+        },
+      }}
+      tokens={stackTokens}
+    >
+      <img className="App-logo" src={logo} alt="logo" />
+      <Text variant="xxLarge" styles={boldStyle}>
+        Welcome to TerraPay
+      </Text>
+
+      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
+        <DefaultButton> Create Account</DefaultButton>
+        <PrimaryButton> Import Account </PrimaryButton>
+      </Stack>
+    </Stack>
+  );
+};
