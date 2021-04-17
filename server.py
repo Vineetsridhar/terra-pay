14559234058eb846c6a6867a11af788478db1d00
Vineet Sridhar <vineets1600@gmail.com>
2021-04-17 15:10:11 -0400

    Added basic new user function

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from flask import Flask, send_from_directory, request
 from dotenv import load_dotenv, find_dotenv
 import sqlite3
 
-con = sqlite3.connect('users.db')
+con = sqlite3.connect('users.db', check_same_thread=False)
 cur = con.cursor()
 
 cur.execute('CREATE TABLE IF NOT EXISTS users (name VARCHAR(32), username VARCHAR(32));')
@@ -18,7 +18,19 @@ APP = Flask(__name__, static_folder='./build/static')
 
 @APP.route('/newUser', methods=['POST'])
 def newUser():
-    print(request.data)
+    data = request.json
+    if "name" not in data or "username" not in data:
+        return {'success':False, 'message': "Params missing"}, 400
+    cur.execute('INSERT INTO users (name, username) VALUES ("%s", "%s")' % (data["name"], data["username"]))
+    con.commit()
+    return {'success': True}, 200
+
+@APP.route('/getUsers', methods=['POST'])
+def getUsers():
+    data = cur.execute('SELECT * FROM users')
+    output = [item[0] for item in data] #returns only name
+    return {'success': True, "data":output}, 200
+
 
 @APP.route('/test', methods=['GET'])
 def handler_user_funding():
