7a00619e21196198ec944de6c9f1854ac552c613
Sam <student979@live.com>
2021-04-17 15:43:01 -0400

    add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,50 @@
 import React from "react";
 import { FontWeights, IStackTokens } from "@fluentui/react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { HomePage } from "./pages/homepage/HomePage";
 import "./App.css";
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
 export const App: React.FunctionComponent = () => {
-  return <HomePage />;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
