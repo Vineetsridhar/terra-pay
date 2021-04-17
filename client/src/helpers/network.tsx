b2423319dc55c633ecee5ea2f983ee83df85ec96
Vineet Sridhar <vineets1600@gmail.com>
2021-04-17 16:17:54 -0400

    Added network calls:

---
+++ client/src/helpers/network.tsx
@@ -0,0 +1,32 @@
+
+export function getEndpoint(path: string) {
+    return `http://localhost:8081/${path}`;
+}
+
+function getAuthToken(){
+    //Get the id token from localstorage?
+    const idToken = ""
+    return {'authorization': `Bearer ${idToken}`}
+}
+
+function getCallParams(body: any) {
+    return {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', ...getAuthToken() },
+        body: JSON.stringify(body)
+    };
+}
+
+async function makeCall(callName: string, callParams: any):Promise<any> {
+    return fetch(callName, getCallParams(callParams));
+}
+
+export async function createNewUser(username:string, name:string){
+    const endpoint = getEndpoint('newUser');
+    return makeCall(endpoint, {username, name}).then(response => response.json());
+}
+
+export async function isUsernameUnique(username:string){
+    const endpoint = getEndpoint('isUsernameUnique');
+    return makeCall(endpoint, {username}).then(response => response.json());
+}
