e751ea80ed75e2d8e1a29cdff292983bc5b58a8b
Vineet Sridhar <vineets1600@gmail.com>
2021-04-17 14:55:58 -0400

    Added SQL basics

--- server.py
+++ server.py
@@ -5,11 +5,20 @@
 import os
 from flask import Flask, send_from_directory, request
 from dotenv import load_dotenv, find_dotenv
+import sqlite3
 
-load_dotenv(find_dotenv())  # This is to load your env variables from .env
+con = sqlite3.connect('users.db')
+cur = con.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS users (name VARCHAR(32), username VARCHAR(32));')
+
+load_dotenv(find_dotenv())  
 
 APP = Flask(__name__, static_folder='./build/static')
 
+@APP.route('/newUser', methods=['POST'])
+def newUser():
+    print(request.data)
 
 @APP.route('/test', methods=['GET'])
 def handler_user_funding():
@@ -20,4 +29,5 @@ if __name__ == "__main__":
     APP.run(
         host=os.getenv('IP', '0.0.0.0'),
         port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', "8081")),
+        debug=True
     )
