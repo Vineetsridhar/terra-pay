8c4aeec0f1ebb3617090f6c2ef882cfe03828eaf
Vineet Sridhar <vineets1600@gmail.com>
2021-04-18 10:03:01 -0400

    Added more code for withdrawing

--- client/src/pages/withdrawfunds/WithdrawFunds.tsx
+++ client/src/pages/withdrawfunds/WithdrawFunds.tsx
@@ -1,64 +1,95 @@
 import { Coins, LCDClient, MnemonicKey, MsgSend } from "@terra-money/terra.js";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { globalEmitter } from "../../helpers/emitter";
 import { getPoolAddress } from "../../helpers/network";
 import { CheckoutForm } from "../addfunds/CheckoutForm";
+import { useHistory } from 'react-router-dom'
+import { Text } from "office-ui-fabric-react";
 
 const terra = new LCDClient({
-  URL: "https://tequila-lcd.terra.dev/",
-  chainID: "tequila-0004",
-  gasPrices: new Coins({ uusd: 0.3 }),
-  gasAdjustment: 1.5,
+    URL: "https://tequila-lcd.terra.dev/",
+    chainID: "tequila-0004",
+    gasPrices: new Coins({ uusd: 0.3 }),
+    gasAdjustment: 1.5,
 });
 
 export function WithdrawFunds() {
-    const sendMoney = async (amt:number) => {
+    const history = useHistory();
+
+    const [balance, setBalance] = useState<number>(0);
+    useEffect(() => {
+        getBalance()
+    }, [])
+    async function getBalance() {
+        const address = localStorage.getItem("address");
+        if (!address) {
+            history.push("/");
+            return;
+        }
+        const coinBalances = await terra.bank.balance(address);
+        const usdBalance = coinBalances.get("uusd");
+        if (usdBalance) {
+            const balance = parseFloat(usdBalance.amount.toString()) / 1000000;
+            setBalance(balance);
+        }
+    }
+
+    const sendMoney = async (amt: number) => {
         const mnemonic = localStorage.getItem('mnemonic');
-        const balance = 100;
         const address = await getPoolAddress()
         amt /= 100
         if (amt - 0.3 <= balance && mnemonic != "") {
-          // Try to process transaction
-          const mk = new MnemonicKey({ mnemonic: mnemonic!! });
-          const localAddress = localStorage.getItem("address");
-          const username = localStorage.getItem("username");
-          const wallet = terra.wallet(mk);
-    
-          // create a simple message that moves coin balances
-          if (localAddress && username) {
-            const send = new MsgSend(localAddress, address["address"], {
-              uusd: amt * 1000000,
-            });
-            console.log(send)
-            return wallet
-              .createAndSignTx({
-                msgs: [send],
-                memo: "From" + username,
-              })
-              .then((tx) => {
-                console.log(tx);
-                return terra.tx.broadcast(tx);
-              })
-              .then((result) => {
-                globalEmitter.emit("notification", {
-                  type: "success",
-                  message: "Your money has been sent over the blockchain!",
+            // Try to process transaction
+            const mk = new MnemonicKey({ mnemonic: mnemonic!! });
+            const localAddress = localStorage.getItem("address");
+            const username = localStorage.getItem("username");
+            const wallet = terra.wallet(mk);
+
+            // create a simple message that moves coin balances
+            if (localAddress && username) {
+                const send = new MsgSend(localAddress, address["address"], {
+                    uusd: amt * 1000000,
                 });
-                console.log(`TX hash: ${result.txhash}`);
-              })
-              .catch((err) => {
-                globalEmitter.emit("notification", {
-                  type: "error",
-                  message: "There was an error sending your money",
-                });
-    
-                console.log("ERRRR", err)
-              });
-          }
+                return wallet
+                    .createAndSignTx({
+                        msgs: [send],
+                        memo: "From" + username,
+                    })
+                    .then((tx) => {
+                        console.log(tx);
+                        return terra.tx.broadcast(tx);
+                    })
+                    .then((result) => {
+                        globalEmitter.emit("notification", {
+                            type: "success",
+                            message: "Your money has been sent over the blockchain!",
+                        });
+                        console.log(`TX hash: ${result.txhash}`);
+                    })
+                    .catch((err) => {
+                        globalEmitter.emit("notification", {
+                            type: "error",
+                            message: "There was an error sending your money",
+                        });
+
+                        console.log("ERRRR", err)
+                    });
+            }
         } else {
-          console.log("error");
+            globalEmitter.emit("notification", {
+                type: "error",
+                message: "You don't have enough money",
+            });
+            throw "err"
         }
-      };
-    
-  return <CheckoutForm deposit={false} callback={sendMoney}/>;
+    };
+
+    return (
+        <div style={{display:'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', height:'100%'}}>
+            <Text variant="xxLarge">
+                Your Balance: ${balance.toFixed(2)}
+            </Text>
+            <br/>
+            <CheckoutForm deposit={false} callback={sendMoney} />
+        </div>)
 }
