be337acf255faf240ac96750984fd3c15798a268
Vineet Sridhar <vineets1600@gmail.com>
2021-04-17 20:07:19 -0400

    Added more stripe stuff

--- client/src/pages/addfunds/CheckoutForm.js
+++ client/src/pages/addfunds/CheckoutForm.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { PaymentRequestButtonElement, useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 import { PrimaryButton, TextField } from 'office-ui-fabric-react';
 import { globalEmitter } from '../../helpers/emitter';
+import { getPaymentIntent } from '../../helpers/network';
 
 export const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
 
   const [amountValidated, setAmountValidated] = useState(0);
-  const [paymentIntent, setPaymentIntent] = useState(null);
   const [validated, setValidated] = useState(false);
   const [amount, setAmount] = useState("");
 
@@ -34,10 +34,22 @@ export const CheckoutForm = () => {
     setValidated(true)
   }
 
+  const onClickPay = () => {
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+    const paymentIntent = await getPaymentIntent(amountValidated, paymentMethod);
+    //Capture here
+  }
+
   return (
     <>
       {validated ?
-        <CardElement /> :
+        <>
+          <CardElement />
+          <PrimaryButton onClick={onClickPay} disabled={!(stripe || elements)}>Submit</PrimaryButton>
+        </> :
         <>
           <TextField label="Amount to deposit" value={amount} onChange={onChangeAmount} />
           <PrimaryButton onClick={onSubmitClick}>Submit</PrimaryButton>
